bdb085c87ef81365812babf39bcb1505d44424cc
dnest19 <54817814+dnest19@users.noreply.github.com>
2021-04-06 20:05:02 -0700

    Add files via upload

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        int a = 3;
+        int b = 10;
+        int c = 15;
+        int d = 2;
+        FizzBuzz tst = new FizzBuzz();
+        tst.fizzBuzz(a);
+        tst.fizzBuzz(b);
+        tst.fizzBuzz(c);
+        tst.fizzBuzz(d);
+    }
+}
