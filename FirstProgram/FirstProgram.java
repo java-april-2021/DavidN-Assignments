b7371535aa6ae69dba52b900ad49b5bbed323764
dnest19 <54817814+dnest19@users.noreply.github.com>
2021-04-06 18:16:26 -0700

    Add files via upload

---
+++ FirstProgram/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram{
+    public static void main(String[] args){
+        System.out.println("My Name is David Nestor");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Placentia, CA");s
+    }
+}
