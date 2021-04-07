e3f4b483858c7d3f18148c98d89d3c512970d63d
dnest19 <54817814+dnest19@users.noreply.github.com>
2021-04-06 18:16:59 -0700

    Update FirstProgram.java

--- FirstProgram/FirstProgram.java
+++ FirstProgram/FirstProgram.java
@@ -2,6 +2,6 @@ public class FirstProgram{
     public static void main(String[] args){
         System.out.println("My Name is David Nestor");
         System.out.println("I am 31 years old");
-        System.out.println("My hometown is Placentia, CA");s
+        System.out.println("My hometown is Placentia, CA");
     }
 }
